5786c23fc339d376ebf4a8ffdbc45d437fee55f5
Waqas Sarwar <waqas.sarwar@coeus-solutions.de>
2022-12-09 20:29:02 +0500

    initial commit

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "DEF",
+    name: "DEFPackage",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "DEF",
+            name: "DEFPackage",
             targets: ["DEF"]),
     ],
     dependencies: [
@@ -18,11 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "DEF",
-            dependencies: []),
-        .testTarget(
-            name: "DEFTests",
-            dependencies: ["DEF"]),
+        .binaryTarget(name: "DEF", path: "./Sources/DEF.xcframework")
     ]
 )
---
+++ Sources/DEF.xcframework/ios-arm64/DEF.framework/Headers/DEF.h
@@ -0,0 +1,18 @@
+//
+//  DEF.h
+//  DEF
+//
+//  Created by Coeus on 09/12/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DEF.
+FOUNDATION_EXPORT double DEFVersionNumber;
+
+//! Project version string for DEF.
+FOUNDATION_EXPORT const unsigned char DEFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DEF/PublicHeader.h>
+
+
